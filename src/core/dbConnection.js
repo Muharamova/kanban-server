a7659e50f76b9185fcd6aa01c576d284f3cb001a
Ilvira Mukharamova <140986540+Muharamova@users.noreply.github.com>
2023-09-17 21:18:11 -0700

    Update dbConnection.js

--- src/core/dbConnection.js
+++ src/core/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function dbConnect() {
-    mongoose.connect('mongodb+srv://Admin:R4dmHscpn1M51t7e@cluster0.slhycaz.mongodb.net/', {useNewUrlParser: true})
+    mongoose.connect('mongodb+srv://name:<parol>@cluster0.slhycaz.mongodb.net/', {useNewUrlParser: true})
         .then(() => console.log('Mongo conection Ok'))
         .catch(err => console.log(err))
 }
